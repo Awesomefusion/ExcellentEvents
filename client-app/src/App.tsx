30900683d5fb82874f1536abc25f1fc267ef799a
Awesomefusion <alexmould@hotmail.co.uk>
2021-01-15 00:23:35 +0000

    Updated React App

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
+import 'semantic-ui-css/semantic.min.css'
+import { Header, Icon } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    values: []
+  }
+
+  componentDidMount(){
+    // api calls
+  }
+
+  // main app content
+  render(){
+    return(
+      <div>
+
+      </div>
+    );
+  }
+
 }
 
 export default App;
